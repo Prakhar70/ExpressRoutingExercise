ee5508c877f76f2f742bcb917aea48e3abadc490
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-09-08 12:59:22 +0530

    Configuration Layer Separation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const dotenv = require('dotenv');
-
-//Start reading the env variable from  .env file
-dotenv.config();
+const { PORT } = require('./config/server.config');
 
 const app = express();
 
-const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
-    console.log(`Started server at  ${PORT}`);
+    console.log(`Started server at  ${PORT} ${EMAIL}`);
 });
